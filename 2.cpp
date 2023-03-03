13f39934151f9ca35301130c663fcaedcd9d339e
DaNiLA243 <danstg217@gmail.com>
2023-03-03 10:25:46 +0000

    wrote function min, delete useless include math

--- 2.cpp
+++ 2.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-#include <math>
 
 using namespace std;
+
+//створюю функцію для порівняння елементів. Повертає менший з них
+int min(int min_el,int i);
 
 int main()
 {
@@ -20,9 +22,16 @@ int main()
     int min_el = 0;
     for(int i = 1; i < n; i++)
     {
-        min_el = min(min_el, a[i])
+        min_el = min(min_el, a[i]);
     }
     
     // виведення максимального елемента
     cout << min_el << endl;
+}
+
+//реалізую функцію min
+int min(int min_el,int i){
+    if (min_el>i){
+        return i;
+    }
 }
