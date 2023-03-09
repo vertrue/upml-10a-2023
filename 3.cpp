05f6f26c0e1ddfcf3cb40122024040d87c6c4962
Yulia3316 <126869641+Yulia3316@users.noreply.github.com>
2023-03-09 21:04:42 +0200

    Update 3.cpp
    Поправила цикл for і умову if

--- 3.cpp
+++ 3.cpp
@@ -1,3 +1,10 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
 #include <iostream>
 
 using namespace std;
@@ -16,12 +23,11 @@ int main()
     }
     
     // кількість нулів у масиві
-    int zeros = 0
-    for(int i = 0; i <= n; i++)
+    int zeros = 0;
+    for(int i = 0; i < n; i++)
     {
-        if(a[i] == 0)
-        {
-            zeros += 2;
+        if(a[i] == 0) {
+            zeros += 1;
         }
     }
     
