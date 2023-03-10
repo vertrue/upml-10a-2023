eff1451547a6efe644d579257a5ec47b36bacf03
Liub4ik <126868129+Liub4ik@users.noreply.github.com>
2023-03-10 11:05:16 +0200

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -16,7 +16,7 @@ int main()
     }
     
     // сума непарних елементів
-    int summa = 0
+    int summa = 0;
     for(int i = 0; i <= n; i++)
     {
         if(a[i] % 2 == 0)
@@ -26,5 +26,5 @@ int main()
     }
     
     // виведення
-    cout << summa << endl
+    cout << summa << endl;
 }
