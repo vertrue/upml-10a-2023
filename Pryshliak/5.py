d27801dbc6ed10fd8165df8aab60e834a20a2b58
Yuri <124350356+GitYuri228@users.noreply.github.com>
2023-04-28 11:23:32 +0300

    cum it

--- Pryshliak/5.py
+++
@@ -1,9 +0,0 @@
-n=int(input())
-a=[];
-for i in range(n):
-    a.append(int(input()))
-summa=0
-for l in range(n):
-    if(a[l]%2==0):
-        summa+=a[l]
-print(summa)
---
+++ Pryshliak/Mine.py
@@ -0,0 +1,7 @@
+from module1 import v10
+
+n = int(input())
+a = []
+for i in range(n):
+    a.append(int(input()))
+print(v10(n, a))
