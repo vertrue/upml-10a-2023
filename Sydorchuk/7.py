3c6a8041fbc297ee1bb6a0869ed8d3a2fe292193
BorysSydorchuk <126240720+BorysSydorchuk@users.noreply.github.com>
2023-04-28 11:33:38 +0300

    splitted 7.py

--- Sydorchuk/7.py
+++ Sydorchuk/7.py
@@ -1,3 +1,5 @@
+import functions as func
+
     # розмір масиву
 n = int(input())
 a = []
@@ -5,13 +7,8 @@ a = []
     # введення масиву
 for i in range(n):
     a.append(int(input()))
-    # сума елементів на парних місцях (нумерація з 0)
 summa = int(0)
-for i in range(n):
-    if i % 2 == 0:
-        summa += a[i]
-        
-    
-    # виведення
-print(summa)
+
+    # виведення сума елементів на парних місцях (нумерація з 0)
+print(func.summa(n, a))
 
